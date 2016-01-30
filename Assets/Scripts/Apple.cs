e1636412abe73a4d185fe5879309ceb29ed62f34
BigDzik0 <bigdzik0@gmail.com>
2016-01-30 22:15:33 +0100

    apple animations

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class Apple : MonoBehaviour {
+
+    public AudioSource sfx;
+    public AudioClip pac;
+
+
+
+
+    public void PlayPac()
+    {
+        sfx.PlayOneShot(pac);
+    }
+
+}
