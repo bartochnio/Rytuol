5fd184729bcd04fee1c044ac6524ec61d1c9db34
kzyg <kamil.zygmuntowicz@gmail.com>
2016-01-30 04:40:11 +0100

    Added sprite sorter.

---
+++ Assets/Scripts/SpriteSorter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SpriteSorter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        SpriteRenderer render = GetComponent<SpriteRenderer>();
+        render.sortingOrder = (int)(transform.position.y * -10);
+    }
+}
