e6cb63b5dfadd22ca6612f88190abe21346c6b33
kzyg <kamil.zygmuntowicz@gmail.com>
2016-01-31 13:31:44 +0100

    Outline script

---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent ( typeof ( OutLine))]
+public class MouseHover : MonoBehaviour {
+
+    OutLine outL;
+	// Use this for initialization
+	void Start () {
+
+        outL = GetComponent<OutLine>();
+	
+	}
+
+    void OnMouseEnter()
+    {
+        outL.drawOutline = true;
+    }
+
+    void OnMouseExit()
+    {
+        outL.drawOutline = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/OutLine.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent ( typeof ( SpriteRenderer))]
+public class OutLine : MonoBehaviour {
+
+    public bool drawOutline = false;
+    public float outlineScale = 2.2f;
+    public float zOffset = 1;
+    public Color outlineColor = Color.black;
+    private SpriteRenderer srcRend;
+    private SpriteRenderer outRend;
+
+    SpriteRenderer CreateOutlinRend()
+    {
+
+        srcRend = GetComponent<SpriteRenderer>();
+
+        GameObject obj = new GameObject("Outline", typeof(SpriteRenderer));
+        obj.transform.SetParent(this.transform);
+        obj.transform.localPosition = Vector3.zero + Vector3.forward * zOffset;
+        SpriteRenderer outRend = obj.GetComponent<SpriteRenderer>();
+        outRend.material = srcRend.material;
+        outRend.sprite = srcRend.sprite;
+
+        return outRend;
+
+    }
+
+    void ModifyOutline(SpriteRenderer rend)
+    {
+        rend.material.color = outlineColor;
+        Vector3 scale = srcRend.transform.localScale;
+        rend.transform.localScale = scale * outlineScale;
+        outRend.sortingOrder = srcRend.sortingOrder - 1;
+    }
+	// Use this for initialization
+	void Start () {
+
+        outRend = CreateOutlinRend();
+        ModifyOutline(outRend);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        outRend.enabled = drawOutline;
+        if (drawOutline)
+        {
+            ModifyOutline(outRend);
+        }
+	
+	}
+}
