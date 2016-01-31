854bcf4cb1181bf303dba21d85b5fbabc7c9003e
BigDzik0 <bigdzik0@gmail.com>
2016-01-31 02:18:29 +0100

    savage animation

---
+++ Assets/Scripts/SavageAn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SavageAn : MonoBehaviour {
+
+
+    Animator anim;
+    Critter crit; 
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        crit = GetComponent<Critter>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (crit.velocity.x > 0 && crit.velocity.y > 0)
+            anim.SetTrigger("UR");
+        if (crit.velocity.x < 0 && crit.velocity.y > 0)
+            anim.SetTrigger("UL");
+        if (crit.velocity.x < 0 && crit.velocity.y < 0)
+            anim.SetTrigger("DL");
+        if (crit.velocity.x > 0 && crit.velocity.y < 0)
+            anim.SetTrigger("DR");
+
+    }
+}
